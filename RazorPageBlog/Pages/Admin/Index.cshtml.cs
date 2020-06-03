e0915b43d5dc17123c92713b33872287b14e85d1
林宏宇 <k86gundam@gmail.com>
2020-06-03 22:23:52 +0800

    Admin後台首頁實作 (顯示Blog文章列表)

--- RazorPageBlog/Pages/Admin/Index.cshtml.cs
+++ RazorPageBlog/Pages/Admin/Index.cshtml.cs
@@ -1,17 +1,40 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPageBlog.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using X.PagedList;
 
 namespace RazorPageBlog.Pages.Admin
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly RazorPageBlogDbContext _context;
+        private readonly int _pageSize;
 
+        public IndexModel(RazorPageBlogDbContext context)
+        {
+            _context = context;
+            _pageSize = 10;
+        }
+
+        public IEnumerable<Article> Articles { get; set; }
+
+        public async Task OnGetAsync(string q, int? p)
+        {
+            var pageIndex = PageIndex(p);
+            var query= _context.Articles.AsQueryable();
+            if (string.IsNullOrWhiteSpace(q)==false)
+            {
+                query = query.Where(d => d.Title.Contains(q));
+            }
+            Articles = await query.ToPagedListAsync(pageIndex, _pageSize);
+        }
+
+        private static int PageIndex(int? p)
+        {
+            var pageIndex = p.HasValue ? p.Value < 1 ? 1 : p.Value : 1;
+            return pageIndex;
         }
     }
 }
