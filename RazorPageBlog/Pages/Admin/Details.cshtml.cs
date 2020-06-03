277727fe0b55b6c691da19ddff3d778578f2a8f4
林宏宇 <k86gundam@gmail.com>
2020-06-03 22:35:25 +0800

    Admin後台文章明細實作

--- RazorPageBlog/Pages/Admin/Details.cshtml.cs
+++ RazorPageBlog/Pages/Admin/Details.cshtml.cs
@@ -1,17 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPageBlog.Data;
+using System;
+using System.Threading.Tasks;
 
 namespace RazorPageBlog.Pages.Admin
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly RazorPageBlogDbContext _context;
 
+        public DetailsModel(RazorPageBlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public Article Article { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Article = await _context.Articles.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (Article == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
