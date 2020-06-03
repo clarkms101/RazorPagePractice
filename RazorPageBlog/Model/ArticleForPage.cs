edf40787a7655183846bf96d09cd4fb3531aa682
林宏宇 <k86gundam@gmail.com>
2020-06-03 16:56:29 +0800

    Blog首頁 顯示文章資料

---
+++ RazorPageBlog/Model/ArticleForPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPageBlog.Model
+{
+    public class ArticleForPage
+    {
+        public Guid Id { get; set; }
+        
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        public string CoverPhoto { get; set; }
+        
+        [Column(TypeName = "datetime")]
+        public DateTime CreateDate { get; set; }
+        
+        public string Tags { get; set; }
+    }
+}
--- RazorPageBlog/Pages/Index.cshtml.cs
+++ RazorPageBlog/Pages/Index.cshtml.cs
@@ -5,21 +5,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using RazorPageBlog.Data;
+using RazorPageBlog.Model;
 
 namespace RazorPageBlog.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly RazorPageBlogDbContext _context;
         private readonly ILogger<IndexModel> _logger;
+        private readonly int _articleBodyLength;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IEnumerable<ArticleForPage> Articles { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, RazorPageBlogDbContext context)
         {
             _logger = logger;
+            _context = context;
+            _articleBodyLength = 200;
         }
 
         public void OnGet()
         {
-
+            Articles = _context.Articles.Select(s => new ArticleForPage()
+            {
+                Id = s.Id,
+                CoverPhoto = s.CoverPhoto,
+                Title = s.Title,
+                Body = s.Body.Substring(0, _articleBodyLength),
+                CreateDate = s.CreateDate,
+                Tags = s.Tags
+            }).ToList();
         }
     }
 }
