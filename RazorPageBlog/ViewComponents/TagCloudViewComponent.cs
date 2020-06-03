d59cdb75098c639e8eff8b1fa0532d1ba1c4e779
林宏宇 <k86gundam@gmail.com>
2020-06-03 17:49:27 +0800

    Blog首頁 加入TagViewComponent (Tag查詢框的資料顯示)(ViewImports調整)

---
+++ RazorPageBlog/ViewComponents/TagCloudViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorPageBlog.Data;
+using System.Linq;
+
+namespace RazorPageBlog.ViewComponents
+{
+    public class TagCloudViewComponent : ViewComponent
+    {
+        private readonly RazorPageBlogDbContext _context;
+
+        public TagCloudViewComponent(RazorPageBlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewData.Model = _context.TagCloud.ToDictionary(d => d.Name, d => d.Amount);
+            return View();
+        }
+    }
+}
