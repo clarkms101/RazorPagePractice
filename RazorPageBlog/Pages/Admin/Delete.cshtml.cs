266a1d0e4fe7b03101c067762a9ea714131b3990
林宏宇 <k86gundam@gmail.com>
2020-06-04 14:31:37 +0800

    Admin後台文章刪除實作

--- RazorPageBlog/Pages/Admin/Delete.cshtml.cs
+++ RazorPageBlog/Pages/Admin/Delete.cshtml.cs
@@ -1,17 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPageBlog.Data;
+using System;
+using System.Threading.Tasks;
 
 namespace RazorPageBlog.Pages.Admin
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly RazorPageBlogDbContext _context;
 
+        public DeleteModel(RazorPageBlogDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Article Article { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Article = await _context.Articles.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Article == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Article = await _context.Articles.FindAsync(id);
+
+            if (Article != null)
+            {
+                _context.Articles.Remove(Article);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
         }
     }
 }
