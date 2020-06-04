368fada17c753a7d75122c8f9a7fc44e884b0b6e
林宏宇 <k86gundam@gmail.com>
2020-06-04 14:45:32 +0800

    Blog首頁 文章標題加上點擊顯示完整內容

---
+++ RazorPageBlog/Pages/Detail.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPageBlog.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace RazorPageBlog.Pages
+{
+    public class DetailModel : PageModel
+    {
+        private readonly RazorPageBlogDbContext _context;
+
+        public DetailModel(RazorPageBlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public Article Article { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Article = await _context.Articles.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Article == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
